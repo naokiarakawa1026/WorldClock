d8ecfc411a15319d9270cc6af6d93d66a749609c
Naoki Arakawa <naoki.arakawa1026@gmail.com>
2021-08-03 22:02:46 +0900

    アダプターの作成（ViewBindingの修正）

--- app/src/main/java/com/example/worldclock/TimeZoneActivity.kt
+++ app/src/main/java/com/example/worldclock/TimeZoneActivity.kt
@@ -1,12 +1,35 @@
 package com.example.worldclock
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.worldclock.databinding.ActivityMainBinding
+import com.example.worldclock.databinding.ActivityTimeZoneBinding
 
 class TimeZoneActivity : AppCompatActivity() {
+
+    var _binding : ActivityTimeZoneBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_time_zone)
+
+        _binding = ActivityTimeZoneBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setResult(Activity.RESULT_CANCELED)
+        val list = binding.timeZoneClockList
+        val adapter = TimeZoneAdapter(this)
+        list.adapter = adapter
+
+        list.setOnItemClickListener { parent, view, position, id ->
+            val timeZone = adapter.getItem(position)
+            val result = Intent()
+            result.putExtra("timeZone", timeZone)
+            setResult(Activity.RESULT_OK, result)
+            finish()
+        }
 
     }
 }
