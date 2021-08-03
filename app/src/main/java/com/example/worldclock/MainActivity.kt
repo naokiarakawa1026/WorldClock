8479aff212a165c6e05ae3a1a295fd40b6f83a14
Naoki Arakawa <naoki.arakawa1026@gmail.com>
2021-08-03 20:20:48 +0900

    時刻表示画面の実装

--- app/src/main/java/com/example/worldclock/MainActivity.kt
+++ app/src/main/java/com/example/worldclock/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.worldclock
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.worldclock.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var _binding : ActivityMainBinding? = null
+    val binding get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val timezone = TimeZone.getDefault()
+        val timeZoneView = binding.timeZone
+        timeZoneView.text = timezone.displayName
+
+        val addButton = binding.add
+        addButton.setOnClickListener {
+            val intent = Intent(this, TimeZoneActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
